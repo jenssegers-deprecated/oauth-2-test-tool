0be57c2bbe6270abbb04ce0e53b9480334b85339
Jens Segers <segers.jens@gmail.com>
2012-07-16 15:24:38 +0200

    Typo

--- view.php
+++ view.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Oauth 2 test tool</title>
+    <title>OAuth 2 test tool</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -22,7 +22,7 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="brand" href="<?php echo $site_url; ?>">Oauth 2 test tool</a>
+          <a class="brand" href="<?php echo $site_url; ?>">OAuth 2 test tool</a>
           
             <ul class="nav">
               <li>
