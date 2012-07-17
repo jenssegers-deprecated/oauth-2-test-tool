c7a6cf4a0bb034eb5ca19b73a7748b5dc0a5bf92
Jens Segers <segers.jens@gmail.com>
2012-07-17 14:19:06 +0200

    Additional warning messages

--- system/actions/authorize.php
+++ system/actions/authorize.php
@@ -4,5 +4,9 @@ $params = array();
 $params['client_id'] = $session->client_id;
 $params['response_type'] = 'code';
 $params['redirect_uri'] = $session->callback_url;
+$params['state'] = md5(time());
+
+// save state
+$session->state = $params['state'];
 
 redirect($session->url_authorize, $params);
--- system/actions/callback.php
+++ system/actions/callback.php
@@ -1,3 +1,16 @@
 <?php
 
-$session->request_code = $_GET['code'];
+if (!isset($_GET['state'])) {
+    $warning = 'Did not receive the state parameter';
+} else {
+    if ($_GET['state'] != $session->state) {
+        $warning = 'State parameter did not match';
+    }
+}
+
+if (!isset($_GET['code'])) {
+    $error = 'Did not receive a request code';
+} else {
+    $session->request_code = $_GET['code'];
+}
+    
